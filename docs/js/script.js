83246ecb8edd55a9370c9fd914c4ce76b6493dc7
alecksv <aleck.svetlovich@gmail.com>
2021-07-13 17:43:37 +0300

    commit from six till footer

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,13 +1,4 @@
-const swiper = new Swiper(".header-slider ", {
-  // Optional parameters
-  // direction: "vertical",
-  // loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-
+const header = new Swiper(".header-slider ", {
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next-uniq",
@@ -17,12 +8,47 @@ const swiper = new Swiper(".header-slider ", {
   slidesPerView: 1,
   spaceBetween: 15,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination-header",
     type: "bullets",
     clickable: true,
   },
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
+});
+
+const project = new Swiper(".projects-slider", {
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next-uniq1",
+    prevEl: ".swiper-button-prev-uniq1",
+  },
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 0,
+});
+
+const team = new Swiper(".team-slider", {
+  // Navigation arrows
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination-team",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+const client = new Swiper(".client-slider", {
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next-uniq3",
+    prevEl: ".swiper-button-prev-uniq3",
+  },
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 150,
+  pagination: {
+    el: ".swiper-pagination-client",
+    type: "bullets",
+    clickable: true,
+  },
 });
